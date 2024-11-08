ee1898daf2616a81125b45ae49d23af1f885888d
Daisy <soniahsiao16@gmail.com>
2024-11-08 14:31:49 +0800

    漏掉一個prop

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,4 +1,4 @@
-function FinishScreen({points, maxPossiblePoints}) {
+function FinishScreen({points, maxPossiblePoints, highscore}) {
    const percentage = ( points / maxPossiblePoints) * 100 
    let emoji;
    if (percentage === 100) emoji = "🥇";
